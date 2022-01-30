6f7478a4bc3e9c713ee435ec3e74a56ddc5e9d65
DimaSurdiy <88757113+DimaSurdiy@users.noreply.github.com>
2022-01-30 15:32:16 +0200

    adds completed task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
+
+function onTimeupdate(currentTime) {
+  const timeValue = currentTime.seconds;
+
+  localStorage.setItem('videoplayer-current-time', timeValue);
+}

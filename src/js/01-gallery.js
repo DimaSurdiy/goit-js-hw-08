b1518e576c0a368c4ecbaf519f5618f0d5cee03c
DimaSurdiy <88757113+DimaSurdiy@users.noreply.github.com>
2022-01-30 11:48:01 +0200

    adds completed task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+createGalleryItemsMarkup();
+
+galleryEl.addEventListener('click', onGalleryItemsClick);
+
+function createGalleryItemsMarkup() {
+  const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+        </a>
+    `;
+    })
+    .join('');
+
+  galleryEl.innerHTML = galleryMarkup;
+}
+
+function onGalleryItemsClick(event) {
+  event.preventDefault();
+
+  var lightbox = new SimpleLightbox('.gallery a');
+}
